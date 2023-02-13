7227447e5f8e56b8a809e81a0713fe671369cd63
oyuka <oyuka@LEAP-16s-iMac.local>
2023-02-13 19:47:48 +0800

    tailwindsurah

--- frontend/src/Pages/Blog.jsx
+++ frontend/src/Pages/Blog.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../comp/Header";
+
 
 export const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <Header />
+      <p className="text-[14px]" >10 Secrets for managing a remote team </p>
+    </div>
+  );
 };
 
 export default Blog;
