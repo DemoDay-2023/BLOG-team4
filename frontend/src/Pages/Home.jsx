e1fb20540f23b2a14046d95c4f422fb25b690dd2
mishka <mishka@nest-32s-iMac.local>
2023-02-13 19:45:47 +0800

    2.13

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import Header from "../comp/Header";
+// import React from "react";s
+// import Header from "../comp/Header";
 
-export const Home = () => {
-  return (
-    <div>
-      <p>Home</p>
-      <Header />
-    </div>
-  );
-};
+// export const Home = () => {
+//   return (
+//     <div>
+//       <p>Home</p>
+//       <Header />
+//     </div>
+//   );
+// };
 
-export default Home;
+// export default Home;
+
+export default function example() {
+  return(
+    // <img className="bg-gradient-to-r" src={pic1} alt=""/>
+    <p>aaaaa</p>
+  )
+}
