bd5a1acc19f030067c03c5a782d653d36866507e
Alimaa69 <temuka2171@gmail.com>
2023-02-17 19:19:19 +0800

    post dutuu bga
    postiig data-basetei holboj bgad gargaj irne

--- frontend/src/Pages/Blogpost.jsx
+++ frontend/src/Pages/Blogpost.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Header from '../comp/Header'
+import React from "react";
+import Header from "../comp/Header";
 // import Footer from '../comp/Footer'
 
 export const Blogpost = () => {
   return (
-    <div className="w-screen h-screen bg-white">
+    <div className="w-screen h-screen bg-[#f5f5f5] flex flex-col">
       <Header />
-      <p className="absolute w-[246px] h-[60px] ml-[140px] mt-[160px] font-Mulish font-extrabold text-5xl leading-[60px] flex align-center">Blog posts</p>
-      <div className='absolute w-[480px] h-[29px] ml-[140px] mt-[240px] font-Mulish font-semibold text-[18px] leading-[29px] flex align-center'>Our latest updates and blogs about managing your team</div>
+      <div className="mr-[200px] ml-[200px] mt-[46px] flex justify-start flex-col">
+        <div className="font-Mulish font-extrabold text-5xl leading-[60px] flex align-center">
+        Blog posts
+        </div>
+        <div className="font-Mulish font-semibold text-[18px] leading-[29px] flex align-center text-[#6D7D8B]">
+        Our latest updates and blogs about managing your team
+        </div>
+      </div>
+      <div>
+        
+      </div>
+      <div className="mr-[200px] ml-[200px] flex justify-center">
+        <button className="h-[44px] w-[165px] bg-slate-200 rounded-md">Next</button>
+      </div>
       {/* <Footer /> */}
     </div>
-  )
-}
+  );
+};
