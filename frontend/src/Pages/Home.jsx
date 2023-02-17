aeaad2bacd8e73b04d316f8f5964eeeef099b16f
oyuka <oyuka@LEAP-16s-iMac.local>
2023-02-17 18:50:31 +0800

    HEADiin code

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -6,11 +6,7 @@ export const Home = () => {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
-      <Footer />
-=======
       <p>Home</p>
->>>>>>> 59b2ff6c482ad456d1b67b8988498be970403640
     </div>
   );
 };
